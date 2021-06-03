680698159e1d717d58b95c2a25cba617d1c51d47
Soyoung <cookr341@gmail.com>
2021-06-04 02:19:52 +0900

    task: Modify basic router

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 import styles from './app.module.css';
 import Home from './components/home/home';
 import Picks from './components/picks/picks';
@@ -7,27 +12,27 @@ function App() {
   return (
     <div className={styles.app}>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Cocktail App</Link>
-              </li>
-              <li>
-                <Link to="/picks">Your Picks</Link>
-              </li>
-            </ul>
-          </nav>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact to="/" aria-label="Go home">
+                Cocktail App
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/picks">Your Picks</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/picks">
-              <Picks />
-            </Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/picks">
+            <Picks />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
