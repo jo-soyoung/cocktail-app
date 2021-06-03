6be02937f37c77aa8b3580b5b8f8d03edfef6da1
Soyoung <cookr341@gmail.com>
2021-06-04 02:43:09 +0900

    task: Add footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = props => (
+  <footer> &copy; 2021 Chingu V30 Geckos-04. All rights reserved.</footer>
+);
+
+export default Footer;
