7ffc98978290f9c15fc82cabf65671e1673c9f88
Soyoung <cookr341@gmail.com>
2021-06-14 20:01:02 +0900

    task: Create add button on details page

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import styles from './details.module.css';
+import Button from '../button/button';
 import Ingredient from '../ingredient/ingredient';
 
 const Details = () => {
   const history = useHistory();
   const historyState = history?.location?.state;
   const ingredients = historyState.ingredients;
-  const taste = historyState.taste.replace(/,/g, ' ');
+  const taste = historyState.taste?.replace(/,/g, ' ');
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  //submit to the firebase database
+  const onSubmit = () => {};
+
   return (
     <main>
       <h1>{historyState.name}</h1>
       <p>{taste}</p>
-      <img src={historyState.image} alt={historyState.name} />
+      <div>
+        <img src={historyState.image} alt={historyState.name} />
+        <Button name="Pick" onClick={onSubmit} />
+      </div>
 
       <h2>Ingredients</h2>
       <ul className={styles.ingredients}>
